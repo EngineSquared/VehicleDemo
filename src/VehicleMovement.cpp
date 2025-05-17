b9bbab67cc365a1c69dc55817c097786d5aaeb1c
Lucas Hauszler <lucas.hauszler@epitech.eu>
2025-05-17 23:10:55 +0200

    feat: steering and braking

--- src/VehicleMovement.cpp
+++ src/VehicleMovement.cpp
@@ -16,14 +16,14 @@ void VehicleMovement(ES::Engine::Core &core)
     }
 
     auto throttle = ES::Plugin::Input::Utils::IsKeyPressed(GLFW_KEY_W) ? 1.0f : 0.0f;
+    auto left = ES::Plugin::Input::Utils::IsKeyPressed(GLFW_KEY_A) ? 1.0f : 0.0f;
+    auto right = ES::Plugin::Input::Utils::IsKeyPressed(GLFW_KEY_D) ? 1.0f : 0.0f;
+    auto steer = right - left;
     auto brake = ES::Plugin::Input::Utils::IsKeyPressed(GLFW_KEY_S) ? 1.0f : 0.0f;
     throttle = brake == 1.0f ? 0.0f : throttle;
 
-    if (throttle > 0.0f) {
-        printf("Throttle: %f\n", throttle);
-    }
 
-    controller->SetForwardInput(throttle);
+    controller->SetDriverInput(throttle, steer, brake, 0.0f);
     // TODO: not always activate
     auto &physicsSystem = core.GetResource<ES::Plugin::Physics::Resource::PhysicsManager>().GetPhysicsSystem();
     physicsSystem.GetBodyInterface().ActivateBody(controllerRef.bodyId);
