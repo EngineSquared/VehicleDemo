5d4c8a94d3250cf2b9575a66f45bc2c0335638b3
Lucas Hauszler <lucas.hauszler@epitech.eu>
2025-05-20 22:32:49 +0200

    fix: bad controller movement

--- src/WheeledVehicleControllerMovement.cpp
+++ src/WheeledVehicleControllerMovement.cpp
@@ -5,15 +5,15 @@
 #include "JoltPhysics.hpp"
 
 constexpr int JOYSTICK_ID = 0;
-constexpr int PS5_L2_AXIS = 2;
-constexpr int PS5_R2_AXIS = 5;
+constexpr int PS5_L3_LR_AXIS = 0;
+constexpr int PS5_L3_UD_AXIS = 1;
+constexpr int PS5_L2_TRIGGER_AXIS = 3;
+constexpr int PS5_R2_TRIGGER_AXIS = 4;
 constexpr int PS5_CIRCLE_BUTTON = 1;
 
-// Tested on PS5 controller
+// Tested on PS5 controller on Windows 11
 void WheeledVehicleControllerMovement::operator()(ES::Engine::Core &core) const
 {
-    
-
     if (!ES::Plugin::Input::Utils::IsJoystickPresent(JOYSTICK_ID))
     {
         return;
@@ -33,47 +33,63 @@ void WheeledVehicleControllerMovement::operator()(ES::Engine::Core &core) const
     auto joystickAxes = ES::Plugin::Input::Utils::GetJoystickAxes(JOYSTICK_ID);
     auto joystickButtons = ES::Plugin::Input::Utils::GetJoystickButtons(JOYSTICK_ID);
 
-    if (joystickAxes.size() < 4)
+    if (joystickAxes.size() < 6)
     {
         return;
     }
 
-    for (auto i = 0; i < joystickButtons.size(); ++i)
+    if (joystickButtons.size() < 1)
     {
-        printf("Button %d: %d\n", i, joystickButtons[i]);
+        return;
     }
 
-    for (auto i = 0; i < joystickAxes.size(); ++i)
-    {
-        printf("Axis %d: %f\n", i, joystickAxes[i]);
-    }
+    bool shouldReverse = false;
 
     auto vehicleSpeedJph = rigidBody.body->GetLinearVelocity();
+    auto vehicleRotationJph = rigidBody.body->GetRotation().GetXYZW();
     auto vehicleSpeed = glm::vec3(vehicleSpeedJph.GetX(), 0.0f, vehicleSpeedJph.GetZ());
-    auto vehicleSpeedMagnitude = glm::length(vehicleSpeed);
-    bool shouldReverse = false;
-    if (vehicleSpeedMagnitude <= 0.1f)
-    {
-        shouldReverse = true;
-    }
+    auto vehicleRotation = glm::quat(vehicleRotationJph.GetX(), vehicleRotationJph.GetY(), vehicleRotationJph.GetZ(),
+                                 vehicleRotationJph.GetW());
+    
+    glm::vec3 forward = vehicleRotation * glm::vec3(0.0f, 0.0f, 1.0f);  // Local Z-axis
+
+    // Dot product to determine direction
+    float direction = glm::dot(vehicleSpeed, forward);
+
+    // If dot product is negative, it's moving backwards
+    shouldReverse = direction < 0.0f;
 
     // L2 is throttle and R2 is brake
     // Automatic gearbox : if the car speed is very slow, the brake key is used as reverse, like in most games
-    auto throttle = joystickAxes[1];
-    
-    
+    auto throttle = (joystickAxes[PS5_L2_TRIGGER_AXIS] + 1.0f) / 2.0f;
+    auto brakeForce = (joystickAxes[PS5_R2_TRIGGER_AXIS] + 1.0f) / 2.0f;
+    if (shouldReverse)
+    {
+        throttle -= brakeForce;
+        brakeForce = 0.0f;
+    }
 
-    
-    
+    // L3 is left/right steering
+    auto steering = joystickAxes[PS5_L3_LR_AXIS];
 
-    // auto throttle = forwardForce + reverseForce;
-    // auto steering = leftForce + rightForce;
-
-    // wheeledVehicle.SetDriverInput(throttle, steering, brakeForce, handbrakeForce);
-
-    // if (throttle != 0.0f || steering != 0.0f || brakeForce != 0.0f || handbrakeForce != 0.0f)
+    // Circle is handbrake
+    // auto handbrakeForce = AdjustAxisValue(joystickButtons[PS5_CIRCLE_BUTTON]);
+    // if (handbrakeForce > 0.0f)
     // {
-    //     
-    //     physicsSystem.GetBodyInterface().ActivateBody(rigidBody.body->GetID());
+    //     handbrakeForce = 1.0f;
     // }
+    // else
+    // {
+    //     handbrakeForce = 0.0f;
+    // }
+    auto handbrakeForce = 0.0f;
+
+    wheeledVehicle.SetDriverInput(throttle, steering, brakeForce, handbrakeForce);
+
+    if (throttle != 0.0f || steering != 0.0f || brakeForce != 0.0f || handbrakeForce != 0.0f)
+    {
+        printf("Throttle: %.2f, Steering: %.2f, Brake: %.2f, Handbrake: %.2f\n", throttle, steering, brakeForce,
+               handbrakeForce);
+        physicsSystem.GetBodyInterface().ActivateBody(rigidBody.body->GetID());
+    }
 }
